3de43eddf12f0592dd54c6b719215c34ee9586fb
Hushed Snicker <hushed.snicker@gmail.com>
2016-11-10 00:49:48 -0800

    Added logic adapters. Working on getting all enabled

--- eileen.chatbot.py
+++ eileen.chatbot.py
@@ -3,7 +3,7 @@
 
 # ChatterBot Imports
 from chatterbot import ChatBot
-#from chatterbot.trainers import 
+from chatterbot.trainers import ChatterBotCorpusTrainer
 
 chatbot = ChatBot(
 
@@ -15,7 +15,7 @@ chatbot = ChatBot(
                    # Storage Adapter init
 
                    storage_adapter = 'chatterbot.adapters.storage.JsonFileStorageAdapter',
-                   database = '../eileen.chatterbot.database.json',
+                   database = './eileen.chatterbot.database.json',
 
 
                    # I/O Adapter init
@@ -27,39 +27,44 @@ chatbot = ChatBot(
                    logic_adapters = [
                                     'chatterbot.adapters.logic.MathematicalEvaluation',
                                     'chatterbot.adapters.logic.TimeLogicAdapter',
+##                                    'chatterbot.adapters.logic.ApproximateSentenceMatchAdapter',  # Missing english stopwords corpus
+##                                    'chatterbot.adapters.logic.ClosestMeaningAdapter',            # Missing english stopwords corpus
+##                                    'chatterbot.adapters.logic.MultiLogicAdapter',                # Not sure if needed. Crashes with an error when used
+##                                    'chatterbot.adapters.logic.SentimentAdapter',                 # Makes her only respond with, "Can I have a cup of sugar?"
                                     'chatterbot.adapters.logic.ClosestMatchAdapter'
                                     ]
 
                  )
 
-# Training Function
+# Training Functions
+
+# Trains the chatbot in English
+def train_in_english():
+  chatbot.set_trainer(ChatterBotCorpusTrainer)
+  chatbot.train('chatterbot.corpus.english')
 
 # Get Response Function
 ### Returns response
 ###### From chatterbot import ChatBot
-######### eileen.chatbot = ChatBot('Eileen')
-######### eileen.chatbot.getresponse()
+######### chatbot = ChatBot('Eileen')
+######### chatbot.get_response()
 
 def print_response():
   response = chatbot.get_response(None)
   return response
 
 
-
-
+# Train
+train_in_english()
 
 # Pre-Main Print
 print('Please tell Eileen "Hello!"')
-chatbot.get_response('Hello')
 
 # Main Loop
 
 while True:
   try:
 
-    # Input code here
-
-    # Some garbage test code
     print_response()
 
   except(KeyboardInterrupt, EOFError, SystemExit):
